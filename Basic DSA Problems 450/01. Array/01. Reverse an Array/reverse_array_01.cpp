05758c305e43075f58a3777aebc2d090f226e24d
Shakibyte <141528031+Shakibyte@users.noreply.github.com>
2023-08-06 05:59:04 +0600

    Create reverse_array_01.cpp

---
+++ Basic DSA Problems 450/01. Array/01. Reverse an Array/reverse_array_01.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int reverse_array(int arr[], int n)
+{
+    for(int i=0, j=n-1; j>i; i++,j--)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+int main()
+{
+int n; 
+cin>>n;
+int arr[n];
+for(int i=0; i<n; i++)
+{
+    cin>>arr[i];
+}
+reverse_array(arr,n);
+
+for(int i=0; i<n; i++)
+{
+    cout<<arr[i];
+}
+return 0;
+}
